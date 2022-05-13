7fee5fc30d7148a73cf36bed93c166f8f5da4b43
ArterMidyme <artermid@gmail.com>
2022-05-13 23:00:25 +0300

    Create 2nd.py

---
+++ 2nd.py
@@ -0,0 +1,33 @@
+a = input()
+b = input()
+c = input()
+n = a + b + c
+if n == '>>>':
+    print('cba')
+if n == '===':
+    print('abc\nacb\nbac\nbca\ncab\ncba')    
+if n == '<<<':
+    print('abc')
+if n == '>>=':
+    print('bca\ncba')
+if n == '>><':
+    print('bca')
+if n == '>=<':
+    print('bac\nbca')
+if n == '><<':
+    print('bac')
+if n == '<<=':
+    print('abc\nacb')
+if n == '<<>':
+    print('acb')
+if n == '=>>':
+    print('cab\ncba')
+if n == '=<<':
+    print('abc\nbac')
+if n == '<=>':
+    print('acb\ncab')
+if n == '<>>':
+    print('cab')
+
+ 
+
